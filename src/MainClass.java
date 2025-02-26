15a4ce180e617161d42cb02575120271cfe666e2
Luan Bertozzi Intellij <LuanBertozziS2@hotmai.com>
2025-02-26 14:32:16 -0400

    uploading the first codes

---
+++ src/MainClass.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Calculadora Simples");
+        System.out.println("Digite o primeiro numero");
+        double primeiroNumero = scanner.nextDouble();
+
+        System.out.println("Digite o segundo numero");
+        double segundoNumero = scanner.nextDouble();
+
+        System.out.println("Digite a operação que deseja");
+        char operacaoMatematica = scanner
+    }
+}
