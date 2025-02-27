be5f8af98ec77f9bd65c41a78e2ab10a397bfcf3
Luan Bertozzi Intellij <LuanBertozziS2@hotmai.com>
2025-02-27 13:52:15 -0400

    finished calculator

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,16 +3,39 @@ import java.util.Scanner;
 
 public class MainClass {
     public static void main(String[] args) {
+        // puxo da classe Scanner para a variavel scanner a ação de ler entradas do usuario no console
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Calculadora Simples");
+        // primeiro numero
         System.out.println("Digite o primeiro numero");
-        double primeiroNumero = scanner.nextDouble();
-
+        double firstUserNumber = scanner.nextDouble();
+        // segundo numero
         System.out.println("Digite o segundo numero");
-        double segundoNumero = scanner.nextDouble();
-
+        double secondUserNumber = scanner.nextDouble();
+        //operação que o usuario deseja
         System.out.println("Digite a operação que deseja");
-        char operacaoMatematica = scanner
+        String operation = scanner.next();
+        double result;
+        switch (operation) {
+            case "+":
+                result = firstUserNumber + secondUserNumber;
+                System.out.println("Resultado:" + result);
+                break;
+            case "-":
+                result = firstUserNumber - secondUserNumber;
+                System.out.println("Resultado:" + result);
+                break;
+            case "*":
+                result = firstUserNumber * secondUserNumber;
+                System.out.println("Resultado:" + result);
+                break;
+                default:
+                System.out.println("Está saida não é válida, é possivel fazer operações com:");
+                System.out.println("Somar +");
+                System.out.println("Subtrair -");
+                System.out.println("Multiplicar *");
+        }
+        scanner.close();
     }
 }
